3b5d0f9009739b7fdceee39740e643a328c0589f
Hanuman Boddu <Hanuman.Boddu@godigit.com>
2024-08-06 15:58:51 +0530

    Adding Feature to check If user already has a booking

--- build/index.js
+++ build/index.js
@@ -12,24 +12,29 @@ const clientService_1 = __importDefault(require("./app/Services/clientService"))
 const Kohinoor = new Hotel_1.default(1, "Kohinoor", 5, "Hyderabad");
 const IndianHotel = new Hotel_1.default(2, "IndianHotel", 10, "Bangalore");
 const LemonTree = new Hotel_1.default(3, "LemonTree", 3, "Mumbai");
+const parkHotels = new Hotel_1.default(4, "ParkHotels", 10, "Delhi");
 const hotelService = new hotelService_1.default();
 const bookingService = new bookingService_1.default();
 const clientService = new clientService_1.default();
 hotelService.addHotel(Kohinoor);
 hotelService.addHotel(IndianHotel);
 hotelService.addHotel(LemonTree);
+hotelService.addHotel(parkHotels);
 const Hanuman = new client_1.default(1, "Hanuman", "hanuman@gmail.com", "7569906878");
 const Rohith = new client_1.default(2, "Rohith", "rohith@gmail.com", "9014575479");
 const Ravi = new client_1.default(3, "Ravi", "ravi@gmail.com", "7894561230");
+const Aditya = new client_1.default(4, "Aditya", "aditya@gmail.com", "7894561230");
 clientService.addClient(Hanuman);
 clientService.addClient(Rohith);
 clientService.addClient(Ravi);
+clientService.addClient(Aditya);
 function bookHotel(id, clientId, hotelId, roomNumber, checkInDate, checkOutDate) {
     const hotel = hotelService.getHotelById(hotelId);
     if (hotel === undefined) {
         console.log("Hotel not found");
         return;
     }
+    //if rooms are not available in this hotel please check other hotels with available rooms
     if (hotel.rooms === 0) {
         console.log("No rooms available");
         console.log("Booking failed");
@@ -41,15 +46,23 @@ function bookHotel(id, clientId, hotelId, roomNumber, checkInDate, checkOutDate)
         console.log("Booking failed");
         return;
     }
+    //To check if the client is already booked in the some hotel in the same date range
+    const bookings = bookingService.getAllBookings();
+    const isClientAlreadyBooked = bookings.some(booking => booking.clientId === clientId && booking.checkInDate <= checkOutDate && booking.checkOutDate >= checkInDate);
+    if (isClientAlreadyBooked) {
+        console.log("Client is already booked in the some hotel in the same date range");
+        console.log("Booking failed");
+        return;
+    }
     const booking = new Booking_1.default(id, clientId, hotelId, roomNumber, checkInDate, checkOutDate);
     bookingService.addBooking(booking);
     hotel.rooms = hotel.rooms - roomNumber;
     hotelService.updateHotel(hotelId, hotel);
     console.log("Booking done successfully");
 }
-// bookHotel(1,1,1,1,new Date(2024,8,6),new Date(2024,8,10));
-bookHotel(2, 2, 3, 3, new Date(2024, 8, 6), new Date(2024, 8, 10));
-bookHotel(3, 3, 3, 1, new Date(2024, 8, 6), new Date(2024, 8, 10));
-bookHotel(4, 3, 3, 1, new Date(2024, 8, 6), new Date(2024, 8, 10));
+// bookHotel(2,2,3,3,new Date(2024,8,6),new Date(2024,8,10));
+// bookHotel(3,3,3,1,new Date(2024,8,6),new Date(2024,8,10));
 // console.log(bookingService.getAllBookings());
 // console.log(hotelService.getAllHotels());
+bookHotel(4, 4, 4, 1, new Date(2024, 8, 6), new Date(2024, 8, 10));
+bookHotel(5, 4, 1, 1, new Date(2024, 8, 8), new Date(2024, 8, 9));
--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import ClientService from "./app/Services/clientService";
 const Kohinoor = new Hotel(1, "Kohinoor", 5, "Hyderabad");
 const IndianHotel = new Hotel(2, "IndianHotel", 10, "Bangalore");
 const LemonTree = new Hotel(3, "LemonTree", 3, "Mumbai");
+const parkHotels=new Hotel(4,"ParkHotels",10,"Delhi");
 
 const hotelService =new HotelService();
 const bookingService =new BookingService();
@@ -16,14 +17,17 @@ const clientService =new ClientService();
 hotelService.addHotel(Kohinoor);
 hotelService.addHotel(IndianHotel);
 hotelService.addHotel(LemonTree);
+hotelService.addHotel(parkHotels);
 
 const Hanuman = new Client(1,"Hanuman","hanuman@gmail.com","7569906878");
 const Rohith = new Client(2,"Rohith","rohith@gmail.com","9014575479");
 const Ravi = new Client(3,"Ravi","ravi@gmail.com","7894561230");
+const Aditya = new Client(4,"Aditya","aditya@gmail.com","7894561230");
 
 clientService.addClient(Hanuman);
 clientService.addClient(Rohith);
 clientService.addClient(Ravi);
+clientService.addClient(Aditya);
 
 function bookHotel(id:number,clientId:number,hotelId:number,roomNumber:number,checkInDate:Date,checkOutDate:Date){
     const hotel = hotelService.getHotelById(hotelId);
@@ -43,6 +47,15 @@ function bookHotel(id:number,clientId:number,hotelId:number,roomNumber:number,ch
         console.log("Booking failed");
         return ;
     }
+
+    //To check if the client is already booked in the some hotel in the same date range
+    const bookings = bookingService.getAllBookings();
+    const isClientAlreadyBooked = bookings.some(booking=>booking.clientId===clientId && booking.checkInDate<=checkOutDate && booking.checkOutDate>=checkInDate);
+    if(isClientAlreadyBooked){
+        console.log("Client already booked in some hotel in the same date range");
+        console.log("Booking failed");
+        return ;
+    }
     
     const booking = new Booking(id,clientId,hotelId,roomNumber,checkInDate,checkOutDate);
     bookingService.addBooking(booking);
@@ -51,8 +64,11 @@ function bookHotel(id:number,clientId:number,hotelId:number,roomNumber:number,ch
     console.log("Booking done successfully");
  }
 
-bookHotel(2,2,3,3,new Date(2024,8,6),new Date(2024,8,10));
-bookHotel(3,3,3,1,new Date(2024,8,6),new Date(2024,8,10));
+// bookHotel(2,2,3,3,new Date(2024,8,6),new Date(2024,8,10));
+// bookHotel(3,3,3,1,new Date(2024,8,6),new Date(2024,8,10));
 
 // console.log(bookingService.getAllBookings());
 // console.log(hotelService.getAllHotels());
+
+bookHotel(4,4,4,1,new Date(2024,8,6),new Date(2024,8,10));
+bookHotel(5,4,1,1,new Date(2024,8,8),new Date(2024,8,9));
